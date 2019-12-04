130f8b6620233b879d9644746f727372c7c7148b
Bentley.Hudson <Bentley.Hudson@ustxgpl4307.us.hsi.local>
2019-12-04 09:07:55 -0600

    customer complaints join on item

--- runaphp.bat
+++ runaphp.bat
@@ -1,3 +1,4 @@
 
-D:\xampp\php\php.exe "D:\xampp\htdocs\MySQLUpdates\delivery_dates.php"
+REM D:\xampp\php\php.exe "D:\xampp\htdocs\MySQLUpdates\custreturnsmergeupdate.php"
+C:\Bitnami\wampstack\php\php.exe "C:\Bitnami\wampstack\apache2\htdocs\MySQLUpdates\custreturnsmergeupdate.php"
 pause
