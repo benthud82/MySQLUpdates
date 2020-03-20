27caa80921e9b3925e4eaa0cba4bad3f79f33a21
Bentley.Hudson <Bentley.Hudson@ustxgpl4307.us.hsi.local>
2020-03-20 12:58:32 -0500

    loc oh update

---
+++ loc_oh.php
@@ -0,0 +1,32 @@
+<?php
+
+ini_set('max_execution_time', 99999);
+ini_set('memory_limit', '-1');
+//include_once '../globalincludes/nahsi_mysql.php';
+include '../connections/conn_slotting.php';
+include_once '../globalincludes/usa_asys.php';
+include_once '../globalincludes/newcanada_asys.php';
+include_once '../heatmap_logic/functions/funct.php';
+
+
+
+
+$result1 = $aseriesconn->prepare("SELECT
+                                    LOWHSE as locoh_whse,
+                                    LOITEM as locoh_item,
+                                    LOLOC# as locoh_loc ,
+                                    LOONHD as locoh_onhand
+                                FROM
+                                    HSIPCORDTA.NPFLOC
+                                WHERE
+                                    LENGTH(RTRIM(TRANSLATE(LOITEM, '*', ' 0123456789'))) = 0");
+$result1->execute();
+$result = $result1->fetchAll(pdo::FETCH_ASSOC);
+$schema = 'slotting';
+$arraychunk = 10000;
+$mysqltable = 'loc_oh';
+
+//insert into table
+pdoMultiInsert_duplicate($mysqltable, $schema, $result, $conn1, $arraychunk);
+
+
