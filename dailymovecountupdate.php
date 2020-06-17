c8e798d0232b646ab601243e071e6267a0e8d09f
Bentley.Hudson <Bentley.Hudson@ustxgpl4307.us.hsi.local>
2020-06-17 09:52:19 -0500

    table

--- dailymovecountupdate.php
+++ dailymovecountupdate.php
@@ -5,9 +5,9 @@ set_time_limit(99999);
 include '../connections/conn_slotting.php';
 
 //INDY
-$sql1 = "DROP TABLE IF EXISTS 2dailymovecount";
-$sql2 = "CREATE TABLE 2dailymovecount(MoveDate DATE, ASOCount INT, AUTOCount INT)";
-$sql3 = "INSERT INTO 2dailymovecount(MoveDate, ASOCount, AUTOCount) SELECT MVDATE, SUM(CASE WHEN MVTYPE IN ('SP','SK','SO','SJ') THEN 1 ELSE 0 END), SUM(CASE WHEN MVTYPE ='RS' THEN 1 ELSE 0 END) FROM 2moves GROUP BY MVDATE";
+$sql1 = "DROP TABLE IF EXISTS slotting.2dailymovecount";
+$sql2 = "CREATE TABLE slotting.2dailymovecount(MoveDate DATE, ASOCount INT, AUTOCount INT)";
+$sql3 = "INSERT INTO slotting.2dailymovecount(MoveDate, ASOCount, AUTOCount) SELECT MVDATE, SUM(CASE WHEN MVTYPE IN ('SP','SK','SO','SJ') THEN 1 ELSE 0 END), SUM(CASE WHEN MVTYPE ='RS' THEN 1 ELSE 0 END) FROM 2moves GROUP BY MVDATE";
     $query = $conn1->prepare($sql1);
     $query->execute();
 	
@@ -22,9 +22,9 @@ $sql3 = "INSERT INTO 2dailymovecount(MoveDate, ASOCount, AUTOCount) SELECT MVDAT
 
 
 //RENO
-$sql4 = "DROP TABLE IF EXISTS 3dailymovecount";
-$sql5 = "CREATE TABLE 3dailymovecount(MoveDate DATE, ASOCount INT, AUTOCount INT)";
-$sql6 = "INSERT INTO 3dailymovecount(MoveDate, ASOCount, AUTOCount) SELECT MVDATE, SUM(CASE WHEN MVTYPE IN ('SP','SK','SO','SJ') THEN 1 ELSE 0 END), SUM(CASE WHEN MVTYPE ='RS' THEN 1 ELSE 0 END) FROM 3moves GROUP BY MVDATE";
+$sql4 = "DROP TABLE IF EXISTS slotting.3dailymovecount";
+$sql5 = "CREATE TABLE slotting.3dailymovecount(MoveDate DATE, ASOCount INT, AUTOCount INT)";
+$sql6 = "INSERT INTO slotting.3dailymovecount(MoveDate, ASOCount, AUTOCount) SELECT MVDATE, SUM(CASE WHEN MVTYPE IN ('SP','SK','SO','SJ') THEN 1 ELSE 0 END), SUM(CASE WHEN MVTYPE ='RS' THEN 1 ELSE 0 END) FROM 3moves GROUP BY MVDATE";
     $query = $conn1->prepare($sql4);
     $query->execute();
 	
@@ -34,9 +34,9 @@ $sql6 = "INSERT INTO 3dailymovecount(MoveDate, ASOCount, AUTOCount) SELECT MVDAT
 	$query = $conn1->prepare($sql6);
     $query->execute();
 //DENVER
-$sql7 = "DROP TABLE IF EXISTS 6dailymovecount";
-$sql8 = "CREATE TABLE 6dailymovecount(MoveDate DATE, ASOCount INT, AUTOCount INT)";
-$sql9 = "INSERT INTO 6dailymovecount(MoveDate, ASOCount, AUTOCount) SELECT MVDATE, SUM(CASE WHEN MVTYPE IN ('SP','SK','SO','SJ') THEN 1 ELSE 0 END), SUM(CASE WHEN MVTYPE ='RS' THEN 1 ELSE 0 END) FROM 6moves GROUP BY MVDATE";
+$sql7 = "DROP TABLE IF EXISTS slotting.6dailymovecount";
+$sql8 = "CREATE TABLE slotting.6dailymovecount(MoveDate DATE, ASOCount INT, AUTOCount INT)";
+$sql9 = "INSERT INTO slotting.6dailymovecount(MoveDate, ASOCount, AUTOCount) SELECT MVDATE, SUM(CASE WHEN MVTYPE IN ('SP','SK','SO','SJ') THEN 1 ELSE 0 END), SUM(CASE WHEN MVTYPE ='RS' THEN 1 ELSE 0 END) FROM 6moves GROUP BY MVDATE";
     $query = $conn1->prepare($sql7);
     $query->execute();
 	
@@ -46,9 +46,9 @@ $sql9 = "INSERT INTO 6dailymovecount(MoveDate, ASOCount, AUTOCount) SELECT MVDAT
 	$query = $conn1->prepare($sql9);
     $query->execute();
 //DALLAS
-$sql10 = "DROP TABLE IF EXISTS 7dailymovecount";
-$sql11 = "CREATE TABLE 7dailymovecount(MoveDate DATE, ASOCount INT, AUTOCount INT)";
-$sql12 = "INSERT INTO 7dailymovecount(MoveDate, ASOCount, AUTOCount) SELECT MVDATE, SUM(CASE WHEN MVTYPE IN ('SP','SK','SO','SJ') THEN 1 ELSE 0 END), SUM(CASE WHEN MVTYPE ='RS' THEN 1 ELSE 0 END) FROM 7moves GROUP BY MVDATE";
+$sql10 = "DROP TABLE IF EXISTS slotting.7dailymovecount";
+$sql11 = "CREATE TABLE slotting.7dailymovecount(MoveDate DATE, ASOCount INT, AUTOCount INT)";
+$sql12 = "INSERT INTO slotting.7dailymovecount(MoveDate, ASOCount, AUTOCount) SELECT MVDATE, SUM(CASE WHEN MVTYPE IN ('SP','SK','SO','SJ') THEN 1 ELSE 0 END), SUM(CASE WHEN MVTYPE ='RS' THEN 1 ELSE 0 END) FROM 7moves GROUP BY MVDATE";
     $query = $conn1->prepare($sql10);
     $query->execute();
 	
@@ -58,9 +58,9 @@ $sql12 = "INSERT INTO 7dailymovecount(MoveDate, ASOCount, AUTOCount) SELECT MVDA
 	$query = $conn1->prepare($sql12);
     $query->execute();
 //JAX
-$sql13 = "DROP TABLE IF EXISTS 9dailymovecount";
-$sql14 = "CREATE TABLE 9dailymovecount(MoveDate DATE, ASOCount INT, AUTOCount INT)";
-$sql15 = "INSERT INTO 9dailymovecount(MoveDate, ASOCount, AUTOCount) SELECT MVDATE, SUM(CASE WHEN MVTYPE IN ('SP','SK','SO','SJ') THEN 1 ELSE 0 END), SUM(CASE WHEN MVTYPE ='RS' THEN 1 ELSE 0 END) FROM 9moves GROUP BY MVDATE";
+$sql13 = "DROP TABLE IF EXISTS slotting.9dailymovecount";
+$sql14 = "CREATE TABLE slotting.9dailymovecount(MoveDate DATE, ASOCount INT, AUTOCount INT)";
+$sql15 = "INSERT INTO slotting.9dailymovecount(MoveDate, ASOCount, AUTOCount) SELECT MVDATE, SUM(CASE WHEN MVTYPE IN ('SP','SK','SO','SJ') THEN 1 ELSE 0 END), SUM(CASE WHEN MVTYPE ='RS' THEN 1 ELSE 0 END) FROM 9moves GROUP BY MVDATE";
     $query = $conn1->prepare($sql13);
     $query->execute();
 	
@@ -70,9 +70,9 @@ $sql15 = "INSERT INTO 9dailymovecount(MoveDate, ASOCount, AUTOCount) SELECT MVDA
 	$query = $conn1->prepare($sql15);
     $query->execute();
 //NOTL
-$sql16 = "DROP TABLE IF EXISTS 11dailymovecount";
-$sql17 = "CREATE TABLE 11dailymovecount(MoveDate DATE, ASOCount INT, AUTOCount INT)";
-$sql18 = "INSERT INTO 11dailymovecount(MoveDate, ASOCount, AUTOCount) SELECT MVDATE, SUM(CASE WHEN MVTYPE IN ('SP','SK','SO','SJ') THEN 1 ELSE 0 END), SUM(CASE WHEN MVTYPE ='RS' THEN 1 ELSE 0 END) FROM 11moves GROUP BY MVDATE";
+$sql16 = "DROP TABLE IF EXISTS slotting.11dailymovecount";
+$sql17 = "CREATE TABLE slotting.11dailymovecount(MoveDate DATE, ASOCount INT, AUTOCount INT)";
+$sql18 = "INSERT INTO slotting.11dailymovecount(MoveDate, ASOCount, AUTOCount) SELECT MVDATE, SUM(CASE WHEN MVTYPE IN ('SP','SK','SO','SJ') THEN 1 ELSE 0 END), SUM(CASE WHEN MVTYPE ='RS' THEN 1 ELSE 0 END) FROM 11moves GROUP BY MVDATE";
     $query = $conn1->prepare($sql16);
     $query->execute();
 	
@@ -82,9 +82,9 @@ $sql18 = "INSERT INTO 11dailymovecount(MoveDate, ASOCount, AUTOCount) SELECT MVD
 	$query = $conn1->prepare($sql18);
     $query->execute();
 //VANC
-$sql19 = "DROP TABLE IF EXISTS 12dailymovecount";
-$sql20 = "CREATE TABLE 12dailymovecount(MoveDate DATE, ASOCount INT, AUTOCount INT)";
-$sql21 = "INSERT INTO 12dailymovecount(MoveDate, ASOCount, AUTOCount) SELECT MVDATE, SUM(CASE WHEN MVTYPE IN ('SP','SK','SO','SJ') THEN 1 ELSE 0 END), SUM(CASE WHEN MVTYPE ='RS' THEN 1 ELSE 0 END) FROM 12moves GROUP BY MVDATE";
+$sql19 = "DROP TABLE IF EXISTS slotting.12dailymovecount";
+$sql20 = "CREATE TABLE slotting.12dailymovecount(MoveDate DATE, ASOCount INT, AUTOCount INT)";
+$sql21 = "INSERT INTO slotting.12dailymovecount(MoveDate, ASOCount, AUTOCount) SELECT MVDATE, SUM(CASE WHEN MVTYPE IN ('SP','SK','SO','SJ') THEN 1 ELSE 0 END), SUM(CASE WHEN MVTYPE ='RS' THEN 1 ELSE 0 END) FROM 12moves GROUP BY MVDATE";
     $query = $conn1->prepare($sql19);
     $query->execute();
 	
@@ -94,9 +94,9 @@ $sql21 = "INSERT INTO 12dailymovecount(MoveDate, ASOCount, AUTOCount) SELECT MVD
 	$query = $conn1->prepare($sql21);
     $query->execute();
 //Calgary
-$sql22 = "DROP TABLE IF EXISTS 16dailymovecount";
-$sql23 = "CREATE TABLE 16dailymovecount(MoveDate DATE, ASOCount INT, AUTOCount INT)";
-$sql24 = "INSERT INTO 16dailymovecount(MoveDate, ASOCount, AUTOCount) SELECT MVDATE, SUM(CASE WHEN MVTYPE IN ('SP','SK','SO','SJ') THEN 1 ELSE 0 END), SUM(CASE WHEN MVTYPE ='RS' THEN 1 ELSE 0 END) FROM 16moves GROUP BY MVDATE";
+$sql22 = "DROP TABLE IF EXISTS slotting.16dailymovecount";
+$sql23 = "CREATE TABLE slotting.16dailymovecount(MoveDate DATE, ASOCount INT, AUTOCount INT)";
+$sql24 = "INSERT INTO slotting.16dailymovecount(MoveDate, ASOCount, AUTOCount) SELECT MVDATE, SUM(CASE WHEN MVTYPE IN ('SP','SK','SO','SJ') THEN 1 ELSE 0 END), SUM(CASE WHEN MVTYPE ='RS' THEN 1 ELSE 0 END) FROM 16moves GROUP BY MVDATE";
     $query = $conn1->prepare($sql22);
     $query->execute();
 	
