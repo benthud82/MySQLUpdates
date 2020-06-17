17269798938cda0d182205149e4f097d89b66d10
Bentley.Hudson <Bentley.Hudson@ustxgpl4307.us.hsi.local>
2020-06-17 07:21:34 -0500

    table

--- 3movescount.php
+++ 3movescount.php
@@ -17,7 +17,7 @@ $tbl_name = "slotting.3moves"; // Table name
 
 include_once '../globalincludes/usa_asys.php';
 
-$excl = $conn1->prepare("SELECT CONCAT(MVITEM,MVTPKG,MVFZNE,MVTZNE,MVTYPE,MVDATE,MVREQT) as EXCLKEY FROM 3moves WHERE MVDATE BETWEEN DATE_SUB(NOW(), INTERVAL 13 DAY) AND NOW()");
+$excl = $conn1->prepare("SELECT CONCAT(MVITEM,MVTPKG,MVFZNE,MVTZNE,MVTYPE,MVDATE,MVREQT) as EXCLKEY FROM slotting.3moves WHERE MVDATE BETWEEN DATE_SUB(NOW(), INTERVAL 13 DAY) AND NOW()");
 $excl->execute();
 $exclarray = $excl->fetchAll(PDO::FETCH_NUM);
 
