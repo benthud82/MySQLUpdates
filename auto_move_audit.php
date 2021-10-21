24942130de51f24fb55f571cebb7fa495a80d3ec
benthud82 <38083671+benthud82@users.noreply.github.com>
2021-10-21 10:37:31 -0500

    auto move audit table

---
+++ auto_move_audit.php
@@ -0,0 +1,53 @@
+<?php
+
+ini_set('max_execution_time', 99999);
+ini_set('memory_limit', '-1');
+//include_once '../globalincludes/nahsi_mysql.php';
+include '../connections/conn_slotting.php';
+include_once '../globalincludes/usa_asys.php';
+include_once '../globalincludes/newcanada_asys.php';
+include_once '../heatmap_logic/functions/funct.php';
+include_once '../CustomerAudit/functions/customer_audit_functions.php';
+
+
+
+
+$result1 = $aseriesconn->prepare("SELECT
+       AUCOMP,
+       AUWHSE,
+       AUBLD# as AUBLD,
+       AUITEM,
+       AUSQTY,
+       AUFLOC,
+       AUTLOC,
+       AUDS2F,
+       AUDS4F,
+       AUDSL2,
+       AUDSL4,
+       AUMTYP,
+       AUESTS,
+       AUEDSP,
+       AUCRDT,
+       AUCRTM,
+       AUPRGM
+FROM
+       HSIPCORDTA.HWAMAUD");
+$result1->execute();
+$result = $result1->fetchAll(pdo::FETCH_ASSOC);
+$schema = 'nahsi';
+$arraychunk = 10000;
+$mysqltable = 'auto_move_audit';
+
+foreach ($result as $key => $value) {
+    $result[$key]['AUCRDT'] = _YYYYMMDDtomysqldate($result[$key]['AUCRDT']);
+
+    $cncltime = str_pad($result[$key]['AUCRTM'], 6, '0', STR_PAD_LEFT);
+    $result[$key]['AUCRTM'] = date('H:i:s', strtotime($cncltime));
+}
+
+
+//$updatecols = array('locoh_onhand','locoh_openalloc','locoh_printalloc');
+//insert into table
+pdoMultiInsert($mysqltable, $schema, $result, $conn1, $arraychunk);
+
+
